bdd64779e156d169b518bcbae85a6ba7d04e039c
Bakunawa0 <danieljuliandado4@gmail.com>
2024-10-24 00:56:22 +0800

    Initial commit. Basic database access

---
+++ mysql_connect.php
@@ -0,0 +1,11 @@
+<?php 
+    $servername = 'localhost';
+    $username = 'root';
+    $dbname = 'movie_ticketing';
+
+    $conn = mysqli_connect($servername, $username, '', $dbname);
+
+    if (!$conn) {
+        die("VBAD: ".mysqli_connect_error());
+    }
+?>
