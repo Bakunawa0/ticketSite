dfabe42b8cd6df9fb58d55f78e2a5a1e1edeab64
Bakunawa0 <danieljuliandado4@gmail.com>
2024-11-28 15:13:35 +0800

    typo in delete_movie

--- delete_movie.php
+++ delete_movie.php
@@ -3,7 +3,7 @@
 include("mysql_connect.php");
 
 $poster = $conn->query("SELECT moviePoster FROM tbl_movies WHERE movieID = ".$_POST['movieID'])->fetch_column(); // so we can delete the poster file
-if ($postDir != "icons/missing.png") // so we don't delete the missing icon
+if ($poster != "icons/missing.png") // so we don't delete the missing icon
     unlink($poster);
 $conn->query("DELETE FROM tbl_movies WHERE movieID = ".$_POST['movieID']);
 
