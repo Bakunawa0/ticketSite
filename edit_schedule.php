75d39fec9700f6994033ef980574ab30781dd1ea
Bakunawa0 <danieljuliandado4@gmail.com>
2024-11-28 08:54:22 +0800

    admin formatting changes

--- edit_schedule.php
+++ edit_schedule.php
@@ -23,74 +23,66 @@
 
         <div style="font-family: monospace;">
             <center>
+                <form method="POST" action="process_new_entry.php" enctype="multipart/form-data">
+                    <fieldset>
+                        <legend><b>Add Film:</b></legend>
+                        <label for="movieName">Title:</label>
+                        <input type="text" required id="movieName" name="movieName"><br>
+                        <label for="moviePoster">Poster (10MiB max):</label>
+                        <input type="file" id="moviePoster" name="moviePoster"><br>
+                        <label for="moviePoster">Ticket Price:</label>
+                        <input type="text" required id="price" name="price"><br>
+                        <label for="rating">Rating:</label>
+                        <select id="rating" name="rating">
+                            <option value="G">G</option>
+                            <option value="PG">PG</option>
+                            <option value="PG13">PG-13</option>
+                            <option value="R16">R-16</option>
+                            <option value="R">R</option>
+                        </select><br>
+                        <label for="timeStart">Time Slot (24H):</label>
+                        <input type="text" required id="timeStart" name="timeStart"><br>
+                        <label for="runTime">Runtime (min):</label>
+                        <input type="text" required id="runTime" name="runTime"><br>
+
+                        <input type="hidden" name="name" value="<?=$_POST['name']?>"> <!-- preserve the username -->
+
+                        <input type="submit" value="Add">
+                    </fieldset>
+                </form>
                 <table>
                     <tr>
-                        <td>
-                            <table>
-                                <tr>
-                                    <th>Time</th>
-                                    <th>Poster</th>
-                                    <th>Film</th>
-                                    <th>Rating</th>
-                                    <th>Edit</th>
-                                </tr>
-                                <?php for ($i = 1; $row = $schedule->fetch_assoc(); $i++) {?>
-                                    <tr>
-                                        <td><?=$row['timeStart'];?></td>
-                                        <td><img height=100 src="<?=$row['moviePoster']?>"></td>
-                                        <td><?=$row['movieName'];?></td>
-                                        <td>[<?=$row['rating'];?>]</td>
-                                        <td>
-                                            <form id="<?=$row['movieID']?>" action="shift_movie.php" method="POST">
-                                                <input type="hidden" name="movieID" value="<?=$row['movieID'];?>">
-                                                <input type="hidden" id="action<?=$row['movieID']?>" name="action" value="">
-                                                <?php if ($i > 1) {?>
-                                                    <input type="image" src="icons/up.png" onclick="shift_movie('up','<?=$row['movieID'];?>')" height=20>
-                                                <?php }?>
-                                                <?php if ($i < $schedule->num_rows) {?>
-                                                    <input type="image" src="icons/down.png" onclick="shift_movie('down','<?=$row['movieID'];?>')" height=20>
-                                                <?php }?>
-                                            </form>
-                                            <form action="delete_movie.php" method="POST" onsubmit="return confirm('Delete <?=$row['movieName'];?>?');">
-                                                <input type="hidden" name="movieID" value="<?=$row['movieID'];?>">
-                                                <input type="hidden" name="amt"value=1>
-                                                <input type="image" src="icons/trash.png" height=20>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                <?php } unset($i);?>
-                            </table>
-                        </td>
-                        <td>
-                            <form method="POST" action="process_new_entry.php" enctype="multipart/form-data">
-                                <fieldset>
-                                    <legend><b>Add Film:</b></legend>
-                                    <label for="movieName">Title:</label>
-                                    <input type="text" required id="movieName" name="movieName"><br>
-                                    <label for="moviePoster">Poster (10MiB max):</label>
-                                    <input type="file" id="moviePoster" name="moviePoster"><br>
-                                    <label for="moviePoster">Ticket Price:</label>
-                                    <input type="text" required id="price" name="price"><br>
-                                    <label for="rating">Rating:</label>
-                                    <select id="rating" name="rating">
-                                        <option value="G">G</option>
-                                        <option value="PG">PG</option>
-                                        <option value="PG13">PG-13</option>
-                                        <option value="R16">R-16</option>
-                                        <option value="R">R</option>
-                                    </select><br>
-                                    <label for="timeStart">Time Slot (24H):</label>
-                                    <input type="text" required id="timeStart" name="timeStart"><br>
-                                    <label for="runTime">Runtime (min):</label>
-                                    <input type="text" required id="runTime" name="runTime"><br>
-
-                                    <input type="hidden" name="name" value="<?=$_POST['name']?>"> <!-- preserve the username -->
-
-                                    <input type="submit" value="Add">
-                                </fieldset>
-                            </form>
-                        </td>
+                        <th>Time</th>
+                        <th>Poster</th>
+                        <th>Film</th>
+                        <th>Rating</th>
+                        <th>Edit</th>
                     </tr>
+                    <?php for ($i = 1; $row = $schedule->fetch_assoc(); $i++) {?>
+                        <tr>
+                            <td><?=$row['timeStart'];?></td>
+                            <td><img height=100 src="<?=$row['moviePoster']?>"></td>
+                            <td><?=$row['movieName'];?></td>
+                            <td>[<?=$row['rating'];?>]</td>
+                            <td style="text-align: center;">
+                                <form id="<?=$row['movieID']?>" action="shift_movie.php" method="POST">
+                                    <input type="hidden" name="movieID" value="<?=$row['movieID'];?>">
+                                    <input type="hidden" id="action<?=$row['movieID']?>" name="action" value="">
+                                    <?php if ($i > 1) {?>
+                                        <input type="image" src="icons/up.png" onclick="shift_movie('up','<?=$row['movieID'];?>')" height=20>
+                                    <?php }?>
+                                    <?php if ($i < $schedule->num_rows) {?>
+                                        <input type="image" src="icons/down.png" onclick="shift_movie('down','<?=$row['movieID'];?>')" height=20>
+                                    <?php }?>
+                                </form>
+                                <form action="delete_movie.php" method="POST" onsubmit="return confirm('Delete <?=$row['movieName'];?>?');">
+                                    <input type="hidden" name="movieID" value="<?=$row['movieID'];?>">
+                                    <input type="hidden" name="amt"value=1>
+                                    <input type="image" src="icons/trash.png" height=20>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php } unset($i);?>
                 </table>
             </center>
         </div>
